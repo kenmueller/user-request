0694f68a5cf470a86e534b01781a1270b4c2f101
kenmueller <kenmueller0@gmail.com>
2020-07-28 04:53:53 -0700

    type documentation

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,27 @@
+/** Request and response headers. */
 export type Headers = Record<string, any>
 
+/**
+ * Request options.
+ * 
+ * - `maxRetryAttempts`: The maximum number of time this request can be retried. Defaults to `10`.
+ * - `headers`: You can override or add request headers.
+ */
 export interface Options {
 	maxRetryAttempts?: number
 	headers?: Headers
 }
 
+/**
+ * Response data.
+ * 
+ * - `data`: The page's HTML content.
+ * - `headers`: The response headers.
+ * - `status`: The status code.
+ * - `statusText`: The status text.
+ * - `retryAttempts`: The number of retry attempts this request took.
+ * - `didFail`: If the request failed by either returning a 404 or 500, or running out of retry attempts.
+ */
 export interface Response {
 	data: string
 	headers: Headers
@@ -14,6 +31,18 @@ export interface Response {
 	didFail: boolean
 }
 
+/**
+ * Fetch a page with the specified URL.
+ * 
+ * @param url The desired page URL.
+ * @param options The request options.
+ * 
+ * @returns A `Promise` of the `Response`. This function never returns a rejected `Promise`.
+ * 
+ * @example
+ * const response = await get('https://memorize.ai')
+ * console.log(response.data)
+ */
 function get(url: string, options?: Options): Promise<Response>
 
 export = get
